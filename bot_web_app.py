e28d45dfc97543ca79f04a159673a3d1c22ea53a
PavelArtemenko <brame1brame@gmail.com>
2024-10-21 23:34:10 +0600

    Сменил h1 tag

---
+++ bot_web_app.py
@@ -0,0 +1,18 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types.web_app_info import WebAppInfo
+
+
+bot = Bot("7718156608:AAFVwr5UPrzA_uBl7q33JaRMcL3LMEukwdI")
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=["start"])
+async def start(message: types.Message):
+
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
+    markup.add(types.KeyboardButton(
+               "Открыть веб страницу", web_app=WebAppInfo(url="/home/aartemenko/Bot/Bot Web App/index.html")))
+
+    await message.answer("Привет, друг!", reply_markup=markup)
+
+executor.start_polling(dp)
