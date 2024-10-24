7727052623e4282fab68ffcab3dd2e3d69f9f811
PavelArtemenko <brame1brame@gmail.com>
2024-10-24 14:46:56 +0600

    commented

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ bot = Bot("7718156608:AAFVwr5UPrzA_uBl7q33JaRMcL3LMEukwdI")
 dp = Dispatcher(bot)
 
 
-@dp.message_handler(commands=["start"])
-async def start(message: types.Message):
+# @dp.message_handler(commands=["start"])
+# async def start(message: types.Message):
 
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
-    markup.add(types.KeyboardButton(
-               "Открыть веб страницу", web_app=WebAppInfo(url="https://apavlon.github.io/Shop/")))
+#     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
+#     markup.add(types.KeyboardButton(
+#                "Открыть веб страницу", web_app=WebAppInfo(url="https://apavlon.github.io/Shop/")))
 
-    await message.answer("Привет, друг!", reply_markup=markup)
+#     await message.answer("Привет, друг!", reply_markup=markup)
 
-executor.start_polling(dp)
+# executor.start_polling(dp)
