7fc14bc293b63846195c1ddc3d67e5707367a8e1
PavelArtemenko <brame1brame@gmail.com>
2024-10-25 13:52:10 +0600

    added @dp.messagehandler@dp.message_handler(content_types=[web_app_data])

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types.web_app_info import WebAppInfo
+import json
 
 
 bot = Bot("7718156608:AAFVwr5UPrzA_uBl7q33JaRMcL3LMEukwdI")
@@ -9,11 +10,16 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=["start"])
 async def start(message: types.Message):
 
-    markup = types.InlineKeyboardMarkup(resize_keyboard=True)
-    markup.add(types.InlineKeyboardButton("Открыть веб страницу",
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add(types.KeyboardButton("Открыть веб страницу",
                web_app=WebAppInfo(url="https://apavlon.github.io/Shop/")))
 
     await message.answer("Привет, друг!", reply_markup=markup)
 
+
+@dp.message_handler(content_types=["web_app_data"])
+async def web_app(message: types.Message):
+    res = json.loads(message.web_app_data.data)
+    await message.answer(f"Name: {res["name"]}. Email: {res["email"]}. Phone: {res["phone"]}")
 
 executor.start_polling(dp)
